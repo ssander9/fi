e3ddf01967c9298217bff89901e9648413725cfe
Sam Anderson <samsand@m-pwg996mc94.local>
2022-09-12 16:18:33 -0400

    added users and sessions

---
+++ app/models/user.rb
@@ -0,0 +1,12 @@
+class User < ApplicationRecord
+  include ActiveModel::SecurePassword
+  has_secure_password
+  
+  has_one_attached :main_image
+  has_many :scenarios, dependent: :destroy
+  has_many :jobs, dependent: :destroy
+  has_many :budgets, dependent: :destroy
+  
+  validates :name, presence: true
+  validates :email, uniqueness: true, presence: true
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,12 @@ Rails.application.routes.draw do
   get '/about', to: 'home#about'
   get '/contact', to: 'home#contact'
 
+  resources :users
+
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  get '/logout', to: 'sessions#destroy'
+
   # Defines the root path route ("/")
   # root "articles#index"
 end
